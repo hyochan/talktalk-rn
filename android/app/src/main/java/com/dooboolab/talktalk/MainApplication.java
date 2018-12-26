853b51d08a6b1e610def414c95d0c2972be5439a
hyochan <dooboolab@gmail.com>
2018-12-26 23:26:19 +0900

    Update unchanged package name in android.

--- android/app/src/main/java/com/dooboolab/talktalk/MainApplication.java
+++ android/app/src/main/java/com/dooboolab/talktalk/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.talktalk.BuildConfig;
 
 import java.util.Arrays;
 import java.util.List;

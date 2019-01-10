59955afa175646a5620fec2a1aefda6c3f02734e
hyochan <dooboolab@gmail.com>
2019-01-10 14:57:26 +0900

    Implemented ProfileUpdate screen with test code.

--- src/components/screen/ProfileUpdate.js
+++ src/components/screen/ProfileUpdate.js
@@ -6,7 +6,9 @@ import {
   Image,
   Text,
   View,
+  ScrollView,
 } from 'react-native';
+import Icon5, { FA5Style } from 'react-native-vector-icons/FontAwesome5';
 
 import type {
   ____ViewStyleProp_Internal as ViewStyle,
@@ -14,40 +16,200 @@ import type {
   ____ImageStyleProp_Internal as ImageStyle,
 } from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 
+import Button from '../shared/Button';
+import TextInput from '../shared/TextInput';
+import { IC_MASK } from '../../utils/Icons';
+
 import { ratio, colors } from '../../utils/Styles';
+import { getString } from '../../../STRINGS';
 
 type Styles = {
   container: ViewStyle,
-}
+  scrollView: ViewStyle,
+  scrollViewContainer: ViewStyle,
+  wrapper: ViewStyle,
+  btnWrapper: ViewStyle,
+  btnLogout: ViewStyle,
+  txtLogout: TextStyle,
+  btnUpdate: ViewStyle,
+  txtUpdate: TextStyle,
+};
 
 const styles: Styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'transparent',
+    backgroundColor: 'white',
     flexDirection: 'column',
     alignItems: 'center',
+  },
+  scrollView: {
+    alignSelf: 'stretch',
+  },
+  scrollViewContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  wrapper: {
+    marginTop: 48,
+    width: '78%',
+
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  btnWrapper: {
+    width: '100%',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 24,
+    marginBottom: 48,
+  },
+  btnLogout: {
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+    borderRadius: 4,
+    borderWidth: 1,
+    width: 136,
+    height: 60,
+    borderColor: colors.dodgerBlue,
+    marginRight: 4,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  txtLogout: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: colors.dodgerBlue,
+  },
+  btnUpdate: {
+    backgroundColor: colors.dodgerBlue,
+    borderColor: colors.dodgerBlue,
+    borderRadius: 4,
+    borderWidth: 1,
+    width: 136,
+    height: 60,
+    marginLeft: 4,
+    shadowColor: colors.dodgerBlue,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowRadius: 4,
+    shadowOpacity: 0.3,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  txtUpdate: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
 
 type Props = {
-
+  navigation: any,
 };
 type State = {
-
+  isUpdating: boolean;
+  displayName: string;
+  statusMsg: string;
+  photoURL: string;
 };
 
 class Screen extends Component<Props, State> {
   static navigationOptions = {
-    title: 'Title',
+    title: getString('MY_PROFILE'),
   };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isUpdating: false,
+      displayName: '',
+      statusMsg: '',
+      photoURL: '',
+    };
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>ProfileUpdate</Text>
+        <ScrollView
+          style={styles.scrollView}
+          contentContainerStyle={styles.scrollViewContainer}
+        >
+          <View style={styles.wrapper}>
+            <TouchableOpacity
+              activeOpacity={0.5}
+              onPress={this.onPressImg}
+            >
+              <Icon5 name="meh" size={80} color={colors.dusk} light/>
+            </TouchableOpacity>
+            <TextInput
+              style={{ marginTop: 80 }}
+              txtLabel={getString('NAME')}
+              txtHint={ getString('NAME') }
+              txt={ this.state.displayName }
+              onTextChanged={ (text) => this.onTextChanged('DISPLAY_NAME', text)}
+            />
+            <TextInput
+              style={{ marginTop: 24 }}
+              txtLabel={getString('STATUS_MSG')}
+              txtHint={ getString('STATUS_MSG') }
+              txt={ this.state.statusMsg }
+              onTextChanged={ (text) => this.onTextChanged('STATUS_MSG', text)}
+            />
+            <View style={styles.btnWrapper}>
+              <Button
+                id='logout'
+                onPress={() => this.onLogout()}
+                style={styles.btnLogout}
+                textStyle={styles.txtLogout}
+              >{getString('LOGOUT')}</Button>
+              <Button
+                id='update'
+                isLoading={this.state.isUpdating}
+                onPress={() => this.onUpdate()}
+                style={styles.btnUpdate}
+                textStyle={styles.txtUpdate}
+              >{getString('UPDATE')}</Button>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     );
+  }
+
+  onLogout = () => {
+    this.props.navigation.navigate('AuthStackNavigator');
+  }
+
+  onUpdate = () => {
+    this.setState({ isUpdating: true }, () => {
+      try {
+        this.props.navigation.goBack();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        this.setState({ isUpdating: false });
+      }
+    });
+  }
+
+  onTextChanged = (type: string, text: string) => {
+    switch (type) {
+      case 'DISPLAY_NAME':
+        this.setState({ displayName: text });
+        break;
+      case 'STATUS_MSG':
+        this.setState({ statusMsg: text });
+        break;
+    }
+  }
+
+  onPressImg = () => {
   }
 }
 
--- src/components/screen/__tests__/ProfileUpdate.test.js
+++ src/components/screen/__tests__/ProfileUpdate.test.js
@@ -15,3 +15,49 @@ describe('rendering test', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
+
+describe('interaction', () => {
+  let props;
+  let wrapper;
+  beforeEach(() => {
+    props = {
+      navigation: {
+        navigate: jest.fn(),
+        goBack: jest.fn(),
+      },
+    };
+    wrapper = shallow(<ProfileUpdate {...props} />);
+  });
+  describe('clicking the button', () => {
+    it('should call onLogout callback', () => {
+      const spy = jest.spyOn(wrapper.instance(), 'onLogout');
+      const btn = wrapper.find('#logout');
+      btn.props().onPress();
+      expect(spy).toHaveBeenCalled();
+      expect(props.navigation.navigate).toHaveBeenCalledWith('AuthStackNavigator');
+    });
+  });
+  describe('onUpdate', () => {
+    it('should navigate when onUpdate callback', () => {
+      const spy = jest.spyOn(wrapper.instance(), 'onUpdate');
+      const btn = wrapper.find('#update');
+      btn.props().onPress();
+      expect(spy).toHaveBeenCalled();
+      expect(wrapper.instance().state.isUpdating).toEqual(false);
+      expect(props.navigation.goBack).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe('change text', () => {
+    it('should change state onTextChanged', () => {
+      const instance = wrapper.instance();
+      instance.onTextChanged('DISPLAY_NAME', 'my_name');
+      expect(instance.state.displayName).toEqual('my_name');
+      instance.onTextChanged('STATUS_MSG', 'aaaaaa');
+      expect(instance.state.statusMsg).toEqual('aaaaaa');
+    });
+  });
+  afterAll(() => {
+    wrapper.instance().prototype.onLogout.mockRestore();
+    wrapper.instance().prototype.onUpdate.mockRestore();
+  });
+});

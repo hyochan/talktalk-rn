2974af2c5714ca9691e5123ef435a97a55f0edaf
TaeSeong Park <imf4@naver.com>
2019-02-02 17:26:10 +0900

    make SearchUser 100% pass on jest test

--- src/components/screen/SearchUser.js
+++ src/components/screen/SearchUser.js
@@ -56,10 +56,11 @@ const styles: Styles = StyleSheet.create({
 const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 type Props = {
-  navigation: any;
+
 };
 type State = {
-  users: Friend[]
+  users: Friend[],
+  searchedUsers: Friend[],
 };
 
 class Screen extends Component<Props, State> {
@@ -68,11 +69,44 @@ class Screen extends Component<Props, State> {
   };
 
   scrollY = new Animated.Value(0);
-  searchTxt: string = '';
-  users: Friend[];
   constructor(props: Props) {
     super(props);
     this.state = {
+      searchedUsers: [
+        {
+          uid: '1',
+          displayName: 'test',
+          photoURL: null,
+          statusMsg: 'status',
+          isOnline: '',
+          friends: '',
+          Chatrooms: '',
+          created: '',
+          updated: '',
+        },
+        {
+          uid: '2',
+          displayName: 'geoseong',
+          photoURL: IC_BACK,
+          statusMsg: 'healthy',
+          isOnline: '',
+          friends: '',
+          Chatrooms: '',
+          created: '',
+          updated: '',
+        },
+        {
+          uid: '3',
+          displayName: 'hyochan',
+          photoURL: IC_ICON,
+          statusMsg: 'healthy',
+          isOnline: '',
+          friends: '',
+          Chatrooms: '',
+          created: '',
+          updated: '',
+        },
+      ],
       users: [
         {
           uid: '1',
@@ -111,9 +145,6 @@ class Screen extends Component<Props, State> {
     };
   }
 
-  componentDidMount = () => {
-    this.users = this.state.users;
-  }
   renderItem = (item: Friend, data: any) => {
     return (
       <UserListItem
@@ -126,21 +157,25 @@ class Screen extends Component<Props, State> {
   showProfileModal = (item: Friend, data: any) => {
     data.actions.showModal(item, true);
   }
-  onItemClick = (item: string) => {
-    this.props.navigation.navigate('Profile', { user: item });
-  }
   onTxtChanged = (txt: string) => {
-    this.searchTxt = txt;
-    this.onSearch();
+    this.onSearch(txt);
   }
-  onSearch = () => {
-    if (this.searchTxt === '') {
-      this.setState({ users: this.users });
-    } else {
-      this.setState({ users: this.users.filter((item) => item.displayName.includes(this.searchTxt)) });
-    }
+  onSearch = (txt: string) => {
+    const searchedUser = (txt === '')
+      ? this.state.searchedUsers
+      : this.state.searchedUsers.filter((item) => item.displayName.includes(txt));
+    this.setState({ users: searchedUser });
   }
 
+  containerStyle = () => {
+    return this.state.users.length === 0
+      ? {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }
+      : null;
+  }
   render() {
     return (
       <ProfileModalConsumer>
@@ -182,18 +217,18 @@ class Screen extends Component<Props, State> {
                         height: 30,
                         top: 10,
                         backgroundColor: 'rgb(247,248,251)',
-                        // backgroundColor: 'white',
                         borderRadius: 4,
                         paddingLeft: 34,
                         paddingRight: 10
                       }}
                       onSubmitEditing={this.onSearch}
-                      defaultValue={this.searchTxt}
+                      defaultValue={''}
                     />
                     <Image source={IC_SEARCH} style={styles.imgSearch}/>
                   </Animated.View>
                 </Animated.View>
                 <AnimatedFlatList
+                  id='animated'
                   style={{
                     width: '100%',
                     height: '100%',
@@ -205,15 +240,7 @@ class Screen extends Component<Props, State> {
                       })
                     }],
                   }}
-                  contentContainerStyle={
-                    this.state.users.length === 0
-                      ? {
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }
-                      : null
-                  }
+                  contentContainerStyle={this.containerStyle}
                   keyExtractor={(item, index) => index.toString()}
                   data={this.state.users}
                   renderItem={({ item }) => this.renderItem(item, data)}
--- src/components/screen/__tests__/SearchUser.test.js
+++ src/components/screen/__tests__/SearchUser.test.js
@@ -1,17 +1,102 @@
 import 'react-native';
 import * as React from 'react';
 import SearchUser from '../SearchUser';
+import { IC_BACK, IC_SEARCH, IC_ICON } from '../../../utils/Icons';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import { shallow, render } from 'enzyme';
 
-describe('rendering test', () => {
+describe('[serachUser] rendering test', () => {
   const wrapper = shallow(
     <SearchUser />,
   );
-
   it('renders as expected', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('[serachUser] interaction', () => {
+  let props;
+  let wrapper;
+  let outer;
+  let context;
+  let animatedFlatList;
+
+  let txtInput;
+
+  const inputData = {
+    uid: '2',
+    displayName: 'geoseong',
+    photoURL: IC_BACK,
+    statusMsg: 'healthy',
+    isOnline: '',
+    friends: '',
+    Chatrooms: '',
+    created: '',
+    updated: '',
+  };
+
+  beforeEach(() => {
+    context = {
+      state: {
+        user: null,
+      },
+      actions: {
+        setModal: jest.fn(),
+        showModal: jest.fn(),
+        onChatPressed: jest.fn(),
+      }
+    }
+    props = {
+      navigation: {
+        navigate: jest.fn(),
+      },
+      ...context,
+    };
+    outer = shallow(<SearchUser {...props} />);
+    const Children = outer.props().children;
+    wrapper = shallow(<Children {...props} />);
+    animatedFlatList = wrapper.find('#animated');
+    txtInput = wrapper.find('TextInput');
+  });
+  describe('[serachUser] when friend name typed in TextInput', () => {
+    
+    it('if there has results: (onTxtChanged -> onSearch) and (renderItem)', () => {
+      const spyOnTxtChanged = jest.spyOn(outer.instance(), 'onTxtChanged');
+      const spyOnSearch = jest.spyOn(outer.instance(), 'onSearch');
+      const spyRenderItem = jest.spyOn(outer.instance(), 'renderItem');
+      
+      animatedFlatList.props().keyExtractor(inputData, 0);
+      animatedFlatList.props().renderItem({ item: inputData });
+      txtInput.props().onChangeText(inputData.displayName);
+
+      expect(spyOnTxtChanged).toHaveBeenCalled();
+      expect(spyOnSearch).toHaveBeenCalled();
+      expect(spyRenderItem).toHaveBeenCalled();
+      expect(outer.instance().state.users[0].displayName).toEqual(inputData.displayName);
+
+      outer.instance().onSearch('');
+      expect(outer.instance().containerStyle()).toEqual(null);
+    });
+    it('if there has no results', () => {
+      outer.instance().onSearch('noresult');
+      expect(outer.instance().state.users.length).toEqual(0);
+      expect(outer.instance().containerStyle()).toEqual({
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      });
+    });
+  });
+  describe('[serachUser] when profile modal clicked', () => {
+    it('should call showProfileModal', () => {
+      const spyShowProfileModal = jest.spyOn(outer.instance(), 'showProfileModal');
+      const UserListItem = animatedFlatList.props().renderItem;
+      const userListItemWrapper = shallow(<UserListItem {...props} />);
+
+      userListItemWrapper.props().onPress();
+      expect(spyShowProfileModal).toHaveBeenCalled();
+    });
   });
 });

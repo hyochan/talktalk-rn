64d9931f28d8c00cd0984658a47584710c3f4aeb
hyochan <dooboolab@gmail.com>
2019-03-11 06:31:57 +0900

    [Chat] test with react-native-testing-library
    Removed enzyme.

--- src/components/screen/__tests__/Chat.test.js
+++ src/components/screen/__tests__/Chat.test.js
@@ -1,65 +1,72 @@
 import 'react-native';
 import * as React from 'react';
-import Chat from '../Chat';
-import { User } from '../../../models/User';
-import { Chat as ChatM } from '../../../models/Chat';
+import Chat, { styles } from '../Chat';
+import  { getString } from '../../../../STRINGS';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import { shallow, render } from 'enzyme';
+import { render, fireEvent } from 'react-native-testing-library';
 
-describe('rendering test', () => {
-  const wrapper = shallow(
-    <Chat />,
-  );
+const props = {
+  navigation: {
+    navigate: jest.fn(),
+  },
+};
+const component: React.Element<any> = <Chat {...props}/>;
 
+describe('[Chat] rendering test', () => {
   it('renders as expected', () => {
-    expect(wrapper).toMatchSnapshot();
+    const json = renderer.create(component).toJSON();
+    expect(json).toMatchSnapshot();
   });
 });
 
-// describe('interaction', () => {
-//   let props
-//   let wrapper;
-//   beforeEach(() => {
-//     props = {
-//       navigation: {
-//         navigate: jest.fn(),
-//         goBack: jest.fn(),
-//       },
-//     };
-//     wrapper = shallow(<Chat {...props} />);
-//   });
-//   it('should render chat item', () => {
-//     const spy = jest.spyOn(wrapper.instance(), 'renderItem');
-//     const item = new ChatM(
-//       '',
-//       new User('sender_uid1', 'dooboolab', '', 'I am fine', true),
-//       'How are you?',
-//     );
-//     wrapper.instance().renderItem({ item });
-//     expect(spy).toHaveBeenCalled();
-//   });
-//   it('should call sendChat', () => {
-//     const spy = jest.spyOn(wrapper.instance(), 'sendChat');
-//     wrapper.instance().sendChat();
-//     expect(spy).toHaveBeenCalled();
-//   });
-//   it('should call showMenu', () => {
-//     const spy = jest.spyOn(wrapper.instance(), 'showMenu');
-//     wrapper.instance().showMenu();
-//     expect(spy).toHaveBeenCalled();
-//   });
-//   it('should navigate when goBack is called', () => {
-//     const spy = jest.spyOn(wrapper.instance(), 'goBack');
-//     wrapper.instance().goBack();
-//     expect(spy).toHaveBeenCalled();
-//     expect(props.navigation.goBack).toHaveBeenCalled();
-//   });
-//   afterAll(() => {
-//     wrapper.instance().prototype.renderItem.mockRestore();
-//     wrapper.instance().prototype.sendChat.mockRestore();
-//     wrapper.instance().prototype.showMenu.mockRestore();
-//     wrapper.instance().prototype.goBack.mockRestore();
-//   });
-// });
+describe('[Login] interaction', () => {
+  let rendered: renderer.ReactTestRenderer;
+  let root: renderer.ReactTestInstance;
+  let testingLib: any;
+
+  beforeAll(() => {
+    rendered = renderer.create(component);
+    root = rendered.root;
+    testingLib = render(component);
+  });
+
+  it('should invoke changeText event handler when message changed', () => {
+    const textInput = testingLib.getByTestId('input_chat');
+    fireEvent.changeText(textInput, 'chat test');
+    expect(textInput.props.value).toEqual('chat test');
+  });
+
+  it('should call [setShowMenu] when focused', () => {
+    const textInput = testingLib.getByTestId('input_chat');
+    textInput.props.onFocus();
+  });
+
+  it('should [showMenu] when touch pressed', () => {
+    let touchMenu = testingLib.getByTestId('touch_menu');
+    fireEvent(touchMenu, 'press');
+
+    touchMenu = testingLib.getByTestId('touch_menu');
+    fireEvent(touchMenu, 'press');
+  });
+
+  it('should call [setShowMenu] when focused', () => {
+    const touchMenu = testingLib.getByTestId('touch_menu');
+    fireEvent(touchMenu, 'press');
+
+    const textInput = testingLib.getByTestId('input_chat');
+    textInput.props.onFocus();
+  });
+
+  it('should [sendChat] when pressing button', () => {
+    let chatBtn = testingLib.getByTestId('btn_chat');
+    fireEvent(chatBtn, 'press');
+
+    const touchMenu = testingLib.getByTestId('touch_menu');
+    fireEvent(touchMenu, 'press');
+
+    chatBtn = testingLib.getByTestId('btn_chat');
+    fireEvent(chatBtn, 'press');
+  });
+});

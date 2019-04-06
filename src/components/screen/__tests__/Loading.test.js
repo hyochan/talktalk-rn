9b45ef938b84be8a6520586e710fbd0a3c4d1007
unolife92 <unolife92@gmail.com>
2019-04-06 23:21:51 +0900

    Migrate [Loading]

--- src/components/screen/__tests__/Loading.test.js
+++ src/components/screen/__tests__/Loading.test.js
@@ -2,16 +2,23 @@ import 'react-native';
 import * as React from 'react';
 import Loading from '../Loading';
 
+import { getString } from '../../../../STRINGS';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import { shallow, render } from 'enzyme';
+ipmort { render, fireEvent } from 'react-native-testing-library';
 
-describe('rendering test', () => {
-  const wrapper = shallow(
-    <Loading />,
-  );
+const props = {
+  navigation: {
+    navigate: jest.fn(),
+  },
+};
 
+const component: React.Element<any> = <Loading {...props}/>;
+
+describe('[Loading] rendering test', () => {
   it('renders as expected', () => {
-    expect(wrapper).toMatchSnapshot();
+    const json = renderer.create(component).toJSON();
+    expect(json).toMatchSnapshot();
   });
 });

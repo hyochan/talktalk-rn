b40f40c5752e7c341940b19deee545a9f93dbe60
TaeSeong Park <imf4@naver.com>
2019-04-06 22:33:02 +0900

    test code migration

--- src/components/screen/__tests__/SearchUser.test.js
+++ src/components/screen/__tests__/SearchUser.test.js
@@ -1,18 +1,21 @@
 import { Animated } from 'react-native';
 import * as React from 'react';
 import SearchUser from '../SearchUser';
-import { IC_BACK, IC_SEARCH, IC_ICON } from '../../../utils/Icons';
+import { IC_BACK } from '../../../utils/Icons';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import { render, fireEvent } from 'react-native-testing-library';
+import { RenderAPI, render, shallow, fireEvent, waitForElement } from 'react-native-testing-library';
 
 const props = {
   navigation: {
     navigate: jest.fn(),
   },
+  onTxtChanged: jest.fn(),
+  onSearch: jest.fn(),
 };
 const component: React.Element<any> = <SearchUser {...props}/>;
+// const component: React.Element<any> = <SearchUser {...props}/>;
 
 describe('[SearchUser] rendering test', () => {
   it('renders as expected', () => {
@@ -22,100 +25,68 @@ describe('[SearchUser] rendering test', () => {
 });
 
 // jest.useFakeTimers(); // related to Animated Component
-// describe('[serachUser] interaction', () => {
-//   let props;
-//   let wrapper;
-//   let outer;
-//   let context;
-//   let animatedFlatList;
-//   let txtInput;
-//   const inputData = {
-//     uid: '2',
-//     displayName: 'geoseong',
-//     photoURL: IC_BACK,
-//     statusMsg: 'healthy',
-//     isOnline: '',
-//     friends: '',
-//     Chatrooms: '',
-//     created: '',
-//     updated: '',
-//   };
-
-//   beforeEach(() => {
-//     jest.useFakeTimers(); // related to Animated Component
-//     context = {
-//       state: {
-//         user: null,
-//       },
-//       actions: {
-//         setModal: jest.fn(),
-//         showModal: jest.fn(),
-//         onChatPressed: jest.fn(),
-//       }
-//     }
-//     props = {
-//       navigation: {
-//         navigate: jest.fn(),
-//       },
-//       ...context,
-//     };
-//     outer = shallow(<SearchUser {...props} />);
-//     const Children = outer.props().children;
-//     wrapper = shallow(<Children {...props} />);
-//     animatedFlatList = wrapper.find('#animatedFlatlist');
-//     txtInput = wrapper.find('#styledInput');
-//   });
-//   describe('when friend name typed in TextInput', () => {
-//     it('if there has results: (onTxtChanged -> onSearch) and (renderItem)', () => {
-//       const spyOnTxtChanged = jest.spyOn(outer.instance(), 'onTxtChanged');
-//       const spyOnSearch = jest.spyOn(outer.instance(), 'onSearch');
-//       const spyRenderItem = jest.spyOn(outer.instance(), 'renderItem');
-      
-//       animatedFlatList.props().keyExtractor(inputData, 0);
-//       animatedFlatList.props().renderItem({ item: inputData });
-//       txtInput.props().onChangeText(inputData.displayName);
-
-//       expect(spyOnTxtChanged).toHaveBeenCalled();
-//       expect(spyOnSearch).toHaveBeenCalled();
-//       expect(spyRenderItem).toHaveBeenCalled();
-//       expect(outer.instance().state.users[0].displayName).toEqual(inputData.displayName);
-
-//       outer.instance().onSearch('');
-//       expect(outer.instance().getContentContainerStyle()).toEqual(null);
-//     });
-//     it('if there has no results', () => {
-//       outer.instance().onSearch('noresult');
-//       expect(outer.instance().state.users.length).toEqual(0);
-//       expect(outer.instance().getContentContainerStyle()).toEqual({
-//         flex: 1,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//       });
-//     });
-//     it('see Animation is working well', () => {
-//       let scrollY = outer.instance().scrollY;
-//       expect(scrollY).toEqual(new Animated.Value(0));
-
-//       outer.instance().onTxtChanged('');
-//       let afterScrollY = new Animated.Value(0)
-//       Animated.timing(afterScrollY, {
-//         toValue: 100,
-//         duration: 500,
-//       }).start();
-//       setTimeout(() => {
-//         console.log('after scrollY', afterScrollY)
-//         expect(scrollY).toEqual(afterScrollY);
-//       }, 1000)
-//     });
-//   });
-//   describe('when profile modal clicked', () => {
-//     it('should call showProfileModal', () => {
-//       const spyShowProfileModal = jest.spyOn(outer.instance(), 'showProfileModal');
-//       const UserListItem = animatedFlatList.props().renderItem;
-//       const userListItemWrapper = shallow(<UserListItem {...props} />);
-
-//       userListItemWrapper.props().onPress();
-//       expect(spyShowProfileModal).toHaveBeenCalled();
-//     });
-//   });
-// });
+describe('[serachUser] interaction', () => {
+  let testingLib: RenderAPI
+  let txtInputInst: renderer.ReactTestInstance
+  let animatedFlatlistInst: renderer.ReactTestInstance
+  const inputData = {
+    uid: '2',
+    displayName: 'geoseong',
+    photoURL: IC_BACK,
+    statusMsg: 'healthy',
+    isOnline: '',
+    friends: '',
+    Chatrooms: '',
+    created: '',
+    updated: '',
+  };
+  
+  beforeAll(() => {
+    // jest.useFakeTimers(); // related to Animated Component
+    testingLib = render(component);
+    txtInputInst = testingLib.getByTestId('txtInput')
+    animatedFlatlistInst = testingLib.getByTestId('animatedFlatlist')
+  });
+  it('when friend name typed in TextInput: (onTxtChanged -> onSearch) and (renderItem)', () => {
+    // setTimeout called - 0
+    fireEvent.changeText(txtInputInst, inputData.displayName)
+    // setTimeout called - 2
+    expect(animatedFlatlistInst.props.data[0]).toEqual(inputData);
+    
+    expect(animatedFlatlistInst.props.contentContainerStyle()).toEqual(null);
+    fireEvent.changeText(txtInputInst, 'noresult!')
+    // setTimeout called - 3
+    expect(animatedFlatlistInst.props.data.length).toEqual(0);
+    expect(animatedFlatlistInst.props.contentContainerStyle()).toEqual({
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    });
+    
+    fireEvent.changeText(txtInputInst, '')
+    // setTimeout called - 4
+    expect(animatedFlatlistInst.props.data.length).toEqual(9);
+  });
+  it('see Animation is working well', () => {
+    jest.useFakeTimers(); // related to timer
+    let scrollY = animatedFlatlistInst.props.testObj.scrollY;
+    let afterScrollY = new Animated.Value(0)
+    Animated.timing(afterScrollY, {
+      toValue: 100,
+      duration: 500,
+    }).start();
+    // setTimeout called - 5
+    setTimeout(() => {
+      console.log('after scrollY', afterScrollY)
+      expect(scrollY).toEqual(afterScrollY);  // doesn't work
+    }, 600);
+    // setTimeout called - 6
+  });
+  
+  it('when profile modal clicked -> should call showProfileModal', () => {
+    fireEvent.changeText(txtInputInst, inputData.displayName);
+    let userListItemInst: renderer.ReactTestInstance = testingLib.getByTestId('userListItem0');
+    fireEvent.press(userListItemInst);
+    let { profileModal } = userListItemInst.props.testObj;  // try to detect cotnext, but undefined
+  });
+});

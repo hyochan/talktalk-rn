b9e5abd15e3387386634f2c88c3f534fee15f9db
Mars Kim <marsinearth@gmail.com>
2019-05-06 12:59:44 +0900

    test error fix

---
+++ __mocks__/react-native-animatable.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Image, Text } from 'react-native';
+
+const createAnimatableComponent = (Component) => (
+  class AnimatableComponent extends React.Component {
+    render() {
+      return <Component/>;
+    }
+  }
+);
+
+const initializeRegistryWithDefinitions = jest.fn();
+
+export {
+  createAnimatableComponent,
+  initializeRegistryWithDefinitions,
+  Image,
+  Text,
+};
